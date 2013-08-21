b28635e54a85893fd407b3fb1ed578484921222c
NanoMolinaCav <NanoMolinaCav@gmail.com>
2013-08-21 05:22:37 +0000

    subiendo archivos

---
+++ adder/sumador.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all; --función (+) y UNSIGNED
+
+ENTITY sumador IS
+    PORT (a: IN std_logic_vector(31 DOWNTO 0);
+          b: IN std_logic_vector(31 DOWNTO 0);
+          y: OUT std_logic_vector(31 DOWNTO 0));
+END sumador;
+
+ARCHITECTURE sum_estructure OF sumador IS
+BEGIN
+
+    PROCESS (a, b) IS
+    BEGIN
+        y <= std_logic_vector(UNSIGNED(a) + UNSIGNED(b));
+    END PROCESS;
+END sum_estructure;
