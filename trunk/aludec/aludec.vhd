17ddf1f89f21ec143a5db153e4975fadfe152c33
NanoMolinaCav@gmail.com <NanoMolinaCav@gmail.com@a62cd452-406c-a442-e655-08fbb093ae1d>
2013-08-28 15:56:24 +0000

    empezando aludec

---
+++ trunk/aludec/aludec.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity aludec is
+    port (funct: in std_logic_vector(5 downto 0);
+          aluop: in std_logic_vector(1 downto 0);
+          alucontrol: out std_logic_vector(2 downto 0));
+end entity;
+
+architecture arq_aludec of aludec is
+begin
+    alucontrol <= "010" when aluop = "00" else
+                  "110" when aluop = "01" else
+                  "010" when aluop(0) = ''
