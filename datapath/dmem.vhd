2d0152ba0d19cee74675796999d11e1165b33999
NanoMolinaCav <NanoMolinaCav@gmail.com>
2013-09-11 14:52:45 +0000

    modificaciones datapath

--- datapath/dmem.vhd
+++ datapath/dmem.vhd
@@ -1,36 +1,78 @@
---   -a --ieee=synopsis -fexplicit -Wc,-m32 -Wa,--32
---   -e -Wa,--32 -Wl,-m32
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+-------------------------------------------------------------------------------
+--
+-- Title       : dmem
+-- Design      : Mips
+-- Author      : Eduardo Sanchez
+-- Company     : Famaf
+--
+-------------------------------------------------------------------------------
+--
+-- File        : dmem.vhd
+--
+-------------------------------------------------------------------------------
+--
+-- Description : Archivo con el diseño de la memoria RAM del procesador MIPS.
+-- Para mantener un diseño corto, la memoria solo contiene 64 palabras de 
+-- 32 bits c/u (aunque podria direccionar mas memoria)
+-- dump: si esta señal esta activa (1), se copia le contenido de la memoria
+-- en el archivo de salida DUMP (para su posterior revision).
+-------------------------------------------------------------------------------
 
-entity dmem is
-    port (a: in std_logic_vector(31 downto 0);
-          wd: in std_logic_vector(31 downto 0);
-          clk, we: in std_logic;
-          rd: out std_logic_vector(31 downto 0));
-end entity;
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use STD.TEXTIO.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
 
-architecture arq_dmem of dmem is
-    type mem is array (0 to 63) of std_logic_vector(31 downto 0);
+library WORK;
+use WORK.components.all;
+
+entity dmem is -- data memory
+  port(clk, we:  in STD_LOGIC;
+       a, wd:    in STD_LOGIC_VECTOR(31 downto 0);
+       rd:       out STD_LOGIC_VECTOR(31 downto 0);
+       dump: in STD_LOGIC
+		 );
+end;
+
+architecture behave of dmem is
+ --constant MEMORY_DUMP_FILE: string := "output.dump";
+ constant MAX_BOUND: Integer := 64;
+ 
+ type ramtype is array (MAX_BOUND-1 downto 0) of STD_LOGIC_VECTOR(31 downto 0);
+ signal mem: ramtype;
+
+procedure memDump is
+  file dumpfile : text open write_mode is MEMORY_DUMP_FILE;
+  variable dumpline : line;
+  variable i: natural := 0;
+  begin
+		write(dumpline, string'("Memoria RAM de Mips:"));
+		writeline(dumpfile,dumpline);
+		write(dumpline, string'("Address Data"));
+		writeline(dumpfile,dumpline);
+     while i <= MAX_BOUND-1 loop        
+		  write(dumpline, i);
+		  write(dumpline, string'(" "));
+		  write(dumpline, conv_integer(mem(i)));		  
+		  writeline(dumpfile,dumpline);
+       i:=i+1;
+     end loop;
+  end procedure memDump;
+
 begin
-    process(clk)
-        variable my_mem: mem;
-        variable address: std_logic_vector(5 downto 0);
-        variable pos_a: integer;
-    begin
-        for pos in 0 to 63 loop --Inicializo mi memoria
-            my_mem(pos) := std_logic_vector(to_unsigned(pos, 32));
-        end loop;
-        if clk'EVENT and clk='1' then
-            address := a(7 downto 2);
-            pos_a := to_integer(unsigned(address));
-            if we='1' then
-                my_mem(pos_a) := wd;
-                rd <= wd;
-            else
-                rd <= my_mem(pos_a);
-            end if;
-        end if;
-    end process;
-end architecture;
+   process(clk, a, mem)
+	begin 
+	  if clk'event and clk = '1' and we = '1' then
+				mem(conv_integer(a(7 downto 2))) <= wd;
+	  end if;
+	   rd <= mem(conv_integer(a(7 downto 2))); -- word aligned
+	end process;
+	
+	process(dump)
+	begin
+	 if dump = '1' then
+	   memDump;
+	 end if;
+	end process;
+
+end;
